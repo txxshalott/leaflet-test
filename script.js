5d1dc1f09eece9df343dbc78cff3e9aad2636eee
txxshalott <txxshalott@gmail.com>
2024-11-14 16:57:01 -0500

    add files

---
+++ script.js
@@ -0,0 +1,39 @@
+const uw = [43.47040812718247, -80.54375993036433];
+
+
+const map = L.map('map').setView(uw, 13);
+
+// Add OpenStreetMap tile layer
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+const marker = L.marker(uw).addTo(map)
+    .bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
+
+
+const polygon = L.polygon([
+    [43.4704704170445, -80.55757867038689],
+    [43.4755935378348, -80.54320203062247],
+    [43.470949913785475, -80.53826959956834],
+    [43.464676574714744, -80.54174174260228]
+]).addTo(map).bindPopup('UWaterloo!');
+
+
+const popup = L.popup()
+    .setLatLng(uw)
+    .setContent('You are here!')
+    .openOn(map);
+
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent(`The coordinates of this place are: ${e.latlng.toString()}`)
+        .openOn(map);
+}
+
+// Add event listener for map clicks
+map.on('click', onMapClick);
